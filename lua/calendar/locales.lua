08cecb11059ce4fdd37665c2457a0c23a1bfcf8e
riodelphino <ryo.heavenlysky@gmail.com>
2026-01-12 12:44:15 +0900

    chore

--- lua/calendar/locales.lua
+++ lua/calendar/locales.lua
@@ -10,7 +10,7 @@ local locales = {
 
   ['de-DE'] = {
     -- stylua: ignore
-    months = { 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember', },
+    months = { 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember' },
     weekdays = { 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So' },
     year_month = function(year, month, months)
       return string.format('%s %d', months[month], year)
@@ -28,7 +28,7 @@ local locales = {
 
   ['es-ES'] = {
     -- stylua: ignore
-    months = { 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre', },
+    months = { 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre' },
     weekdays = { 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom' },
     year_month = function(year, month, months)
       return string.format('%s %d', months[month], year)
@@ -37,7 +37,7 @@ local locales = {
 
   ['fr-FR'] = {
     -- stylua: ignore
-    months = { 'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre', },
+    months = { 'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre' },
     weekdays = { 'lun', 'mar', 'mer', 'jeu', 'ven', 'sam', 'dim' },
     year_month = function(year, month, months)
       return string.format('%s %d', months[month], year)
@@ -46,7 +46,7 @@ local locales = {
 
   ['it-IT'] = {
     -- stylua: ignore
-    months = { 'gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre', },
+    months = { 'gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre' },
     weekdays = { 'lun', 'mar', 'mer', 'gio', 'ven', 'sab', 'dom' },
     year_month = function(year, month, months)
       return string.format('%s %d', months[month], year)
@@ -54,6 +54,7 @@ local locales = {
   },
 
   ['ja-JP'] = {
+    months = nil,
     weekdays = { '月', '火', '水', '木', '金', '土', '日' },
     year_month = function(year, month, _)
       return string.format('%04d 年 %2d 月', year, month)
@@ -62,7 +63,7 @@ local locales = {
 
   ['ko-KR'] = {
     -- stylua: ignore
-    months = { '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월', },
+    months = { '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월' },
     weekdays = { '월', '화', '수', '목', '금', '토', '일' },
     year_month = function(year, month, _)
       return string.format('%04d년 %2d월', year, month)
@@ -70,6 +71,7 @@ local locales = {
   },
 
   ['zh-CN'] = {
+    months = nil,
     weekdays = { '一', '二', '三', '四', '五', '六', '日' },
     year_month = function(year, month, _)
       return string.format('%04d 年 %2d 月', year, month)
@@ -77,6 +79,7 @@ local locales = {
   },
 
   ['zh-TW'] = {
+    months = nil,
     weekdays = { '一', '二', '三', '四', '五', '六', '日' },
     year_month = function(year, month, _)
       return string.format('%04d 年 %2d 月', year, month)
@@ -85,7 +88,7 @@ local locales = {
 
   ['ru-RU'] = {
     -- stylua: ignore
-    months = { 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь', },
+    months = { 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь' },
     weekdays = { 'пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс' },
     year_month = function(year, month, months)
       return string.format('%s %d г.', months[month], year)
